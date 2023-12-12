db2c096ecae6edf8386e37ea6e2b040a6c4d848c
Lukas Schreiber <info@lukasschreiber.com>
2023-12-12 23:28:55 +0100

    remove tests

--- src/test/kotlin/com/lukasschreiber/potlucksheet/PotluckSheetApplicationTests.kt
+++ src/test/kotlin/com/lukasschreiber/potlucksheet/PotluckSheetApplicationTests.kt
@@ -3,11 +3,11 @@ package com.lukasschreiber.potlucksheet
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class PotluckSheetApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class PotluckSheetApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
